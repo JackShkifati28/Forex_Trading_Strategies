adf6a04d9ebf2ad19474b2361bd7d3f4cb171636
Jack Shkifati <jackshkifati@gmail.com>
2026-07-15 17:34:31 -0400

    Install pandas-ta from source

---
+++ strategies/momentum.py
@@ -0,0 +1,8 @@
+from Core.indicator import Indicator
+import numpy as np
+from strategies.base_strategy import BaseStrategy
+
+class mommentum(BaseStrategy):
+
+    def __init__(self):
+        pass
